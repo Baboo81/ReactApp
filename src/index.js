3f52aa5693d1b6d4b1c487d8375554cb8e9c5230
Baboo81 <magicbaboo@gmail.com>
2023-01-03 13:55:17 +0100

    ReactApp

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class App extends React.Component{
+  render(){
+    return (
+      <section id="todo">
+            <h1 className="m3">Liste de tâches</h1>
+            <ul className="list-group m3">
+                <li className="list-group-item d-flex align-tiems-center">
+                    Ranger la vaisselle
+                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button>
+                </li>
+                <li className="list-group-item d-flex align-tiems-center">
+                    Répondre appel d'offres
+                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button>
+                </li>
+                <li className="list-group-item d-flex align-tiems-center">
+                    Signer contrat
+                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button> 
+                </li>
+                <li className="list-group-item d-flex align-tiems-center">
+                    Ranger le salon
+                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button>
+                </li>
+            </ul>
+            <footer className="d-flex justify-content-between bg-secondary p3" id="mainFooter">
+                <div className="btn-group">
+                    <a href="#" className="btn btn-outline-dark bg-light">List</a>
+                    <a href="#" className="btn btn-outline-dark bg-light">Completed</a>
+                    <a href="#" className="btn btn-outline-dark bg-light">Add</a>
+                </div>
+            </footer>
+        </section>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))//Permet d'injecter l'app ds la div root qui se trouve ds la fichier index.html
