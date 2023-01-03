74f5176a9c7f9bf73a9a50538a66b2b18d0d1bb8
Baboo81 <magicbaboo@gmail.com>
2023-01-03 15:10:53 +0100

    modif

--- src/index.js
+++ src/index.js
@@ -1,43 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {FalistAlt, FaCheckSquare, FaPlusSquare, FaTrash} from 'react-icons/fa';
+import App from './components/App';
+import './css/toDo.css';
 
 
-class App extends React.Component{
-  render(){
-    return (
-      <section id="todo">
-            <h1 className="m3">Liste de tâches</h1>
-            <ul className="list-group m3">
-                <li className="list-group-item d-flex align-tiems-center">
-                    Ranger la vaisselle
-                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button>
-                </li>
-                <li className="list-group-item d-flex align-tiems-center">
-                    Répondre appel d'offres
-                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button>
-                </li>
-                <li className="list-group-item d-flex align-tiems-center">
-                    Signer contrat
-                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button> 
-                </li>
-                <li className="list-group-item d-flex align-tiems-center">
-                    Ranger le salon
-                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button>
-                </li>
-            </ul>
-            <footer className="d-flex justify-content-between bg-secondary p3" id="mainFooter">
-                <div className="btn-group">
-                    <a href="#" className="btn btn-outline-dark bg-light"><FalistAlt /></a>
-                    <a href="#" className="btn btn-outline-dark bg-light"><FaCheckSquare /></a>
-                    <a href="#" className="btn btn-outline-dark bg-light"><FaPlusSquare /></a>
-                </div>
-                <button className="btn btn-outline-dark bg-ligth"><FaTrash /></button>
-            </footer>
-        </section>
-    )
-  }
-}
 
-ReactDOM.render(<App />, document.getElementById('root'))//Permet d'injecter l'app ds la div root qui se trouve ds la fichier index.html
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
