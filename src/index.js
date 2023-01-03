a9fd564faa65e02b21da80835cdcc93cffcb0f86
Baboo81 <magicbaboo@gmail.com>
2023-01-03 13:38:58 +0100

    React app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component{
+  render(){
+    return (
+      <section id="todo">
+            <h1 className="m3">Liste de tâches</h1>
+            <ul className="list-group m3">
+                <li className="list-group-item d-flex align-tiems-center">
+                    Ranger la vaisselle
+                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button>
+                </li>
+                <li className="list-group-item d-flex align-tiems-center">
+                    Répondre appel d'offres
+                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button>
+                </li>
+                <li className="list-group-item d-flex align-tiems-center">
+                    Signer contrat
+                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button> 
+                </li>
+                <li className="list-group-item d-flex align-tiems-center">
+                    Ranger le salon
+                    <button className="btn btn-sm ml-auto btn-outline-succes">&#x2713;</button>
+                </li>
+            </ul>
+            <footer className="d-flex justify-content-between bg-secondary p3" id="mainFooter">
+                <div className="btn-group">
+                    <a href="#" className="btn btn-outline-dark bg-light">List</a>
+                    <a href="#" className="btn btn-outline-dark bg-light">Completed</a>
+                    <a href="#" className="btn btn-outline-dark bg-light">Add</a>
+                </div>
+            </footer>
+        </section>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))//Permet d'injecter l'app ds la div root qui se trouve ds la fichier index.html
