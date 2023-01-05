d4a05252565fbe98fb76ca3f8b7bc1462fd41305
Baboo81 <magicbaboo@gmail.com>
2023-01-05 11:22:41 +0100

    Added props, components

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 class AddTask extends React.Component{
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.onAddTask(this.newTask.value)
+        this.props.history.push('/')//Permet de rediriger l'utilisateur vers la racine
+    }
+
     render(){
         return (
             <section>
                 <h1 className="m-3">Nouvelle tâche</h1>
                 <div className="card mx-3">
-                    <form className="card-body">
+                    <form className="card-body" onSubmit={e => this.handleSubmit(e)}>
                         <div className="form-group">
                             <label form="taskName">Nom de la tâche</label>
                             <input type="text" className="form-control" name="taskName" id="taskName" required ref={input => this.newTask = input} />
@@ -20,3 +27,4 @@ class AddTask extends React.Component{
 }
 
 export default AddTask
+//ref permet de récuperer des données du formulaire
--- src/components/App.js
+++ src/components/App.js
@@ -4,32 +4,53 @@ import ToDoList from './ToDoList'
 import AddTask from './AddTask'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import initialData from '../initialData'
+import uniqueid from 'uniqueid'
 
 class App extends React.Component{
 
-    class App extends React.Component{
-
-        state = {
-            tasks: initialData
-        }
-
-        onToggleCompleted = (taskId) => {
-            let taskToUpdate = this.state.tasks.find(task => task.id === taskId)//Permet de trouver la tâche à changer
-            taskToUpdate.completed = !taskToUpdate.completed
-
-            this.setState(prevState => (
-                prevState.tasks.map(task => {
-                    return task.id === taskId ? taskToUpdate : task
-                })
-            ))
-        }
+    state = {
+        tasks: initialData
     }
+
+    onToggleCompleted = (taskId) => {
+        let taskToUpdate = this.state.tasks.find(task => task.id === taskId)
+        taskToUpdate.completed = !taskToUpdate.completed
+
+        this.setState(prevState => (
+            prevState.tasks.map(task => {
+                return task.id === taskId ?  taskToUpdate : task
+            })
+        ))
+    }
+
+    onAddTask = (newTaskName) => {//Permet de créer une nouvelle tâche
+        let newTask = {//Voici l'objet = tâche
+            id: uniqueid(),//c'est le package uniqueid qui va générer l'id
+            name: newTaskName,
+            completed: false
+        }
+
+        this.setState(prevState => ({
+            tasks: [...prevState.tasks, newTask]
+        }))
+    }
+
+    onDeleteCompleted = () => {//Cette fct va modifier l'état de cette app
+        this.setState(prevState => {
+            let newState = prevState.tasks.filter(task => !task.completed)
+            return {
+                tasks: newState
+            }
+        })
+    }
+
+
     render(){
         return(
             <section id="todo">
                 <BrowserRouter>
                     <Switch>
-                        <Route path="/add-task" component={AddTask} />
+                        <Route path="/add-task" render={(props) => <AddTask {...props} onAddTask={this.onAddTask} />} />
                         <Route path="/:filter?" render={(props) => <ToDoList {...props} tasks={this.state.tasks} onToggleCompleted={this.onToggleCompleted} />} />
                     </Switch>
                     <NavBar />
